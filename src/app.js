e2f8a0cb3351558f07ef957a35e6589a445e393c
Leonardo Mercado <lamercadob1@gmail.com>
2022-09-18 11:08:48 -0500

    cambio menor

--- src/app.js
+++ src/app.js
@@ -6,20 +6,28 @@ require('dotenv').config();
 const app = express();
 const authRoutes = require('./routes/auth.route')
 const servicio = require('./routes/servicio.route');
+const reserva = require('./routes/reserva.route');
 
 //middelwares
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
 //CROS
+//CROS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //prefijos o rutas
 app.use('/api/auth', authRoutes);
 app.use('/api/servicio', validateToken, servicio);
-app.use('/api/reserva', validateToken, servicio);
+app.use('/api/reserva',validateToken, reserva);
 
 
-//
 function validateToken(req, res, next){
     const accessToken =req.headers['authorization'] || req.query.accessToken;
     if(!accessToken) res.send('Access denied');
